bd1e407a2a5e404e70238e67d3d418f47378e80f
Utkarsh Kumar <kumarutkarsh2505@gmail.com>
2022-03-29 22:46:00 +0530

    update

--- urban-ladder-clone/src/Redux/Allproduct/productApi.js
+++ urban-ladder-clone/src/Redux/Allproduct/productApi.js
@@ -4,9 +4,10 @@ import { product_failure, product_request, product_success } from "./product.act
 export const allProductApi = (params)=>(dispatch)=>{
 dispatch(product_request())
 axios
-.get("http://localhost:3001/all_product")
+.get("http://localhost:8000/products")
 .then((res)=>{
-    dispatch(product_success(res.data))
+    console.log(res)
+    dispatch(product_success(res.data.products))
 })
 .catch((res)=>{
     dispatch(product_failure())
