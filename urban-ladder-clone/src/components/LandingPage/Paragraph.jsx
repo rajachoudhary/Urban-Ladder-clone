c296b2d03ef61f3eafc95106a55a2d59e22a2afa
rajachoudhary <rajachoudhary.com>
2022-04-01 01:29:48 +0530

    landing page complete

---
+++ urban-ladder-clone/src/components/LandingPage/Paragraph.jsx
@@ -0,0 +1,320 @@
+import React from "react";
+import style from "../../css/paragraph.module.css"
+export const Paragraph = () => {
+  return (
+    <div className={style.paragraphContainer}>
+      <h1>
+        Buy Furniture Online At Urban Ladder - India's Favourite Destination For
+        All Things Home!
+      </h1>
+      <h2>Shop For The Best Furniture Online For Your Home</h2>
+      <p>
+        Furniture is amongst the most crucial elements of a home and more often
+        than not a true reflection of the people living in it. From the style of
+        bed to the colour of the chairs, every little detail makes a massive
+        impact, which is why choosing the right furniture for your home is
+        essential. Tired of searching for furniture shop near me in the search
+        engine of your choice and not finding substantial results? We have just
+        the solution for you. Urban Ladder, an online furniture store, is home
+        to exceptional furniture options for every part of your home including
+        the bedroom, living room, dining room, and more. Trust Urban Ladder with
+        your home furniture needs and enjoy access to premium furniture made
+        from the highest quality materials. When you shop for furniture online
+        from Urban Ladder, you’re assured of a durable product that’ll add
+        immense value to your interior decor.
+      </p>
+      <p>
+        At Urban Ladder, we take our customers’ needs very seriously. A team of
+        furniture designers work tirelessly to craft fresh new furniture designs
+        that cater to the requirements of today’s homeowner flawlessly. We take
+        design aesthetics equally seriously which is why we have furniture
+        options available in different aesthetics including classic, modern,
+        mid-century, boho chic, Scandanavian, industrial, and many more. Buy
+        high-quality furniture for your home without worrying about overspending
+        since our prices can be best described as reasonable. If you’re setting
+        up a home office, you can find the perfect option in our range of study
+        tables. Need a coffee table for your living room? Urban Ladder has got
+        you covered.
+      </p>
+      <h2>Choose From A Wide Range Of Furniture For Your Home</h2>
+      <ul>
+        <li>Bedroom furniture</li>
+        <p>
+          Your bedroom is arguably the one place where the quality of furniture
+          used matters a lot. Without proper bedroom furniture, your personal
+          space will look untidy and poorly organized, which isn’t great both
+          for the quality of your sleep and personal hygiene. Urban Ladder
+          offers fantastic bedroom furniture designs that cover a wide spectrum
+          from old school to modern, without compromising on the quality or
+          comfort. Need a bedroom furniture set? You’re at the right place. We
+          offer convenient bedroom furniture sets to help you avoid the hassle
+          of finding matching sets of bedroom furniture. The key to finding the
+          perfect modern bedroom furniture is understanding the space available
+          and then finding furniture options that fit the space perfectly to
+          create a functional area. Urban Ladder offers premium bedroom
+          furniture to help you do just that. Need kids’ bedroom furniture? We
+          have furniture for your kids’ bedroom in countless fun colours and
+          patterns. Apart from bedroom furniture, Urban Ladder offers a great
+          range of furniture for the rest of your home including couches, dining
+          tables, couches, and more. Our TV unit designs, in particular, are
+          stellar!
+        </p>
+        <li>Living room furniture</li>
+        <p>
+          Your living room is typically where you spend a majority of your
+          waking hours, which makes finding the right living room furniture even
+          more important. Your choice of living room furniture needs to be
+          comfortable, visually appealing, and designed to fit the space
+          available. Urban Ladder offers spectacular living room furniture
+          designs to help you find the perfect option for your living space. No
+          matter the style and size of furniture you need for your living room,
+          Urban Ladder has got you covered. If you don’t want the hassle of
+          individually picking out furniture, our living room furniture sets are
+          tailor-made for you. At Urban Ladder, you don’t have to settle for
+          something substandard. With our reasonable prices, you can bring home
+          luxury living room furniture without the heavy cost generally
+          associated with furniture. From recliners and side tables to sectional
+          sofas and cabinets, our collection of living room furniture has
+          everything you need. End your search for furniture shop near me by
+          shopping from Urban Ladder!
+        </p>
+        <li>Dining room furniture</li>
+        <p>
+          Finding the perfect dining room furniture is essential since the
+          quality of your mealtimes doesn’t just depend on the food but also
+          where you have it. Set up a dream dining room by shopping for
+          high-quality dining room furniture online from Urban Ladder. Our
+          online furniture store is equipped with functional and stunning dining
+          room furniture options designed to uplift your space and make it
+          prettier. No matter the kind of furniture you need, our innovative and
+          well thought-out dining room furniture designs will do the trick.
+          Looking for the latest dining furniture design? You’re at the right
+          place. Urban Ladder offers beautiful modern dining furniture that fits
+          the contemporary home like a glove. All our furniture options are
+          designed by expert craftsmen and made using the finest quality
+          materials to ensure they last for years. Urban Ladder’s collection of
+          furniture is truly inclusive and includes everything from dining
+          chairs and wall cabinets to wooden sofas and coffee tables.
+        </p>
+      </ul>
+      <h2>Explore Urban Ladder’s Furniture Stores For Latest Collection</h2>
+      <p>
+        Sofas, beds, desks, chairs, tables, there’s something so enjoyable about
+        slowly meandering through furniture stores. If you haven’t seen Urban
+        Ladder’s latest collection at our stores yet, you are missing out. Sure,
+        sitting at home and browsing furniture stores online for a few hours is
+        thrilling on its own. But no one can deny the satisfaction of walking
+        into furniture shops and running from one sofa to another, trying this
+        bed and then that or even sitting on cushioned chairs to pick one that
+        is perfect for curling up and sleeping. So whether you want to shop in a
+        store or if you’re looking to buy furniture online, there is only one
+        retailer that stands out as the best— Urban Ladder. And the most fun
+        part? You can buy your furniture online or in a physical store where you
+        can touch and experience the furniture before you make a purchase.
+      </p>
+      <p>
+        Urban Ladder gives you the unique experience of adding a bit of your own
+        quirky style and personality to your home. Our expansive collection
+        gives you an unparalleled in-store or online furniture shopping
+        experience. So, the next time you think, “Where’s the closest furniture
+        shop near me?”, stop and think again. With multiple furniture shops all
+        over India, Urban Ladder makes buying a new chair, sofa, bed, ottoman,
+        or wall decor that matches your personality easy.
+      </p>
+      <p>
+        Dreaming about transforming your home? Just walk into the nearest
+        furniture store, Urban Ladder, only, of course, for inspiration. With
+        myriad options for both home and office, Urban Ladder is an excellent
+        option for anyone who needs durable yet attractive furniture. Being the
+        best furniture online is not an easy feat. From curating the best styles
+        to sourcing innovative designs, Urban Ladder is bustling with unique
+        ways for you to elevate your home. With beautiful, long-lasting, and
+        sustainably sourced materials, each piece that you find at Urban Ladder
+        will fit right in with your current decor. So, the next time you find
+        yourself looking for the best “furniture shop near me”, look no further
+        than the actual best, Urban Ladder.
+      </p>
+      <h2>
+        Urban Ladder Offers Gorgeous Furniture Online To Fuel Your Creativity:
+      </h2>
+      <p>
+        Spend precious weekends to visit 4-5 stores with your family, wait for
+        the staff to attend to you, compare prices, features and discounts,
+        haggle over the ‘final’ price, and finally wonder about the quality of
+        wood and workmanship. Surely there has to be a better way to furnish
+        your dream home? Online furniture shopping is an oasis of calm in this
+        chaotic situation. When you can purchase phones, mutual funds,
+        vacations, and laptops online, why hesitate to buy furniture online? The
+        quality is at par with – or even better than – their offline
+        counterparts. Furniture online is also covered under excellent warranty
+        terms.
+      </p>
+      <p>
+        Online furniture sites offer you a wide variety of options to choose
+        from. You can browse many designs, categories, sizes, functionalities
+        and colours, all at your convenience. Why follow fixed timings when you
+        can browse whenever you want, wherever you are? Shops cannot display
+        more than a fixed number of designs – a constraint never faced
+        digitally. While browsing furniture online, India shows a clear
+        preference for clean, contemporary designs. You can find many such
+        ranges on our website.
+      </p>
+      <p>
+        Yet another advantage of selecting furniture online is that a physical
+        space can never exhibit enough categories or sizes. When you buy
+        furniture online, you can see the features, benefits, and functionality
+        of each product clearly. No more relying on the staff to remember and
+        convey every point – however big or small. You can also sort the
+        products using some parameter, or apply filters as per your needs,
+        compare as many options as you want (without feeling awkward), and add
+        your favourites to your wishlist.
+      </p>
+      <p>
+        While looking for the best place to buy furniture online, India prefers
+        our range without doubt. Whenever you want to buy furniture online, it’s
+        worthwhile to check our offerings first. We are sure you’ll find what
+        you were looking for. You can also buy furniture by room, as every
+        living space has distinct requirements – in terms of occupancy,
+        functions, space, and budget. From opulent, intricate designs for large
+        areas to versatile, multifunction designs for lesser square feet, we
+        have it all.
+      </p>
+      <h2>Learn How To Buy Wooden Furniture Online The Smart Way:</h2>
+      <p>
+        Four Fs are an integral part of every urban home – family, friends, fun,
+        and furniture. Online furniture shopping sites offer a hassle-free way
+        to add warmth to your living spaces. Chosen correctly, wooden furniture
+        online has the potential to transform your home décor. While selecting
+        pure wood furniture online, India usually makes a mental checklist of
+        the obvious and latent needs. This ensures the numerous options of
+        furniture for home online do not disorient the shoppers.
+      </p>
+      <p>
+        Just before you begin the process of online furniture purchase, try
+        imagining each room in your house with the products. If this appears
+        difficult, roughly sketch the top view of the room using pencil and
+        paper. Then try placing each piece of online furniture for home, along
+        with the dimensions. This should give you a fair idea of how much each
+        room can accommodate, and how much walking area it would offer. If it is
+        not possible for the entire family to shop together, you can make a
+        longlist of solid wood furniture online and then consult your family.
+      </p>
+      <p>
+        Understand the functions of every room, the number of people using it at
+        different times – on daily, weekly, and monthly basis, the amount of
+        space available (considering the walking space), and the other interior
+        accessories in the room. For example, while looking at sofa sets, you
+        should consider the number of friends who visit you over the weekend, or
+        the number of moms and kids who come over for playdates, and so on.
+        Another example is the type of chair you buy; i.e. a rocking chair or a
+        recliner to make yourself comfortable at the end of a long day. If you
+        have a compact living room, look for folding / extendable dining tables
+        and sofa-cum-beds within wooden furniture online. For wooden furniture
+        online shopping, India usually selects highly functional products with
+        multiple uses.
+      </p>
+      <p>
+        Once you finalise a shortlist of home furniture online, try consulting
+        your close friends if possible – especially the ones who have
+        beautifully furnished homes. Apart from their sense of aesthetics, you
+        can bank on their understanding of your personality. More often than
+        not, they would have a good idea of what furniture might match your
+        persona the best. To buy furniture online, India usually favours sites
+        that have a good variety of designs, sizes, budgets, and more.
+        Considering the various parameters of furnishing your home, our website
+        is the best place to buy furniture online.
+      </p>
+      <h2>Purchase Furniture Online From The Finest Place Possible:</h2>
+      <p>
+        We offer the most optimum combination of aesthetics, variety,
+        dimensions, functionalities, and prices for online furniture. From
+        modern to traditional designs, minimal to intricate details, muted to
+        vibrant colours, compact to spacious sizes, we offer practically every
+        type of furniture online to match your interior design. You can find
+        solutions for all types of spaces – bedrooms, kitchens, passages, living
+        rooms, dining areas, study, balconies, guest rooms, entry foyers, bars,
+        and outdoors. You can easily get products that are difficult to spot
+        elsewhere during furniture online shopping; for example room dividers,
+        corner storage units, and more.
+      </p>
+      <p>
+        Whether it is a big purchase decision like a bed, or a small one like a
+        chair cushion, our easy EMIs make buying smooth and reasonable for you.
+        A straightforward exchange policy serves as an assurance of our
+        exceptional product quality. Our comprehensive, multi-year warranty also
+        covers the rare instance when our material or workmanship might be found
+        wanting. While selling furniture online, we also unveil special sections
+        that serve specific needs, such as fine dining, deals of the week, new
+        arrivals, and more. All these factors make urbanladder.com the best
+        website for buying furniture online in India. So go ahead and browse
+        multiple categories, sort, filter, compare, and shortlist products,
+        create your wishlist and shopping cart, and furnish your dream home.
+      </p>
+      <h2>One-Stop Solution For All Your Wooden Furniture Needs</h2>
+      <p>
+        Selecting furniture for your home is serious business. There are quite a
+        few things you need to take into consideration before you decide to
+        spruce up your space with new home furniture. When you browse through
+        furniture online, you should have the dimensions of the area where you
+        want to place it. You can then choose from a wide range of wooden
+        furniture in different shapes and colours. You will also find benefits,
+        features, prices and other essential details about the product listed
+        online.
+      </p>
+      <p>
+        While times have changed and people no longer feel the need to walk into
+        a brick and mortar furniture shop to get a feel of the furniture
+        themselves, you’re always free to walk into an Urban Ladder furniture
+        shop nearby. You’ll find a wide range of furniture items for each
+        category and our experts will help you if you choose the right wood
+        furniture for your home based on your requirements. They will take you
+        through the different types of furniture and give you the pros and cons
+        of each so that you have all the information you need to make the right
+        decision. Also, you can choose any furniture that fits the bill
+        perfectly without having to worry about your budget as we offer easy EMI
+        options.
+      </p>
+      <p>
+        Wondering about a furniture shop near me? You may have many furniture
+        stores near your residence, so what’s so unique about Urban Ladder?
+        Offline furniture stores can only have a few exhibits in their showroom,
+        which limits your choice. Now, Urban Ladder has both offline and online
+        stores which means that you can explore the widest selection of
+        furniture at every price range.
+      </p>
+      <p>
+        Check out our dining table range and much more online to spruce up your
+        home.
+      </p>
+      <h2>Check Out The Affordable Furniture Price At Urban Ladder</h2>
+      <p>
+        When it comes to furniture, a lot of people end up settling for
+        something other than what they wanted because of the cost involved. At
+        Urban Ladder, we understand this. To ensure you don’t compromise on your
+        vision for your home, we offer reasonable furniture prices after
+        factoring in the cost of sourcing materials from the finest local
+        establishments and the compensation for our extraordinary craftsman. Is
+        your search for furniture shop near me leading to a deadend? Allow us to
+        help. Urban Ladder offers premium quality home furniture at prices you
+        can afford, to ensure you furnish your home with the best furniture
+        there is. Browse our bed designs, compare the prices, and bring home a
+        super comfortable and stylish bed today.
+      </p>
+      <p>
+        At Urban Ladder, our prices are reasonable for every type of furniture
+        you wish to buy. Our furniture bed prices are pretty standard,
+        especially considering the quality on offer. Even our furniture sofa set
+        prices can be described as accessible given the size of the furniture
+        and it’s build. Trust Urban Ladder with your furniture needs and buy
+        beautiful furniture without worrying about the price. From couches and
+        double beds to chairs and dining tables, all our furniture options are
+        affordable.
+      </p>
+      <div className={style.heading}>SIGN UP AND GET A CHANCE TO WIN THE GIVEAWAY OF THE WEEK.</div>
+      <div className={style.inputContainer}>
+          <input placeholder="Your Email ID"/>
+          <button>SUBSCRIBE</button>
+      </div>
+    </div>
+  );
+};
